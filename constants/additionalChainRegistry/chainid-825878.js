2b83ecab16ad0ed031a2336161ebea35408dd616
Ryder Hiew <ryderhiew@Ryders-MacBook-Pro-2.local>
2025-12-16 00:28:25 +0800

    add hypercluster

---
+++ constants/additionalChainRegistry/chainid-825878.js
@@ -0,0 +1,25 @@
+export const data = {
+  name: "HyperCluster",
+  chain: "HYPEC",
+  rpc: ["https://rpc.hypercluster.org"],
+  faucets: ["https://faucet.hypercluster.org"],
+  nativeCurrency: {
+    name: "HyperCluster Token",
+    symbol: "HYPEC",
+    decimals: 18,
+  },
+  features: [{ name: "EIP155" }, { name: "EIP1559" }],
+  infoURL: "https://www.hypercluster.org/",
+  shortName: "HYPEC",
+  chainId: 825878,
+  networkId: 825878,
+  icon: "https://hypercluster.org/img/favicon.png",
+  explorers: [
+    {
+      name: "HyperCluster Explorer",
+      url: "https://explorer.hypercluster.org",
+      icon: "hypercluster",
+      standard: "EIP3091",
+    },
+  ],
+};
